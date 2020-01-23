1186527074f6657d586522463ed4028175c0d463
Jones Ogolo <jay.topher11@gmail.com>
2020-01-24 00:16:28 +0100

    replaced with apply button component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.scss'
+import ApplyButton from '../ApplyButton/ApplyButton'
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         <h1>
         Over 200,000 loans provided to individuals and small businesses in Nigeria.
         </h1>
-        <a href="#" className="butn">Apply Now</a>
+        <ApplyButton />
       </div>
       
     </div>
