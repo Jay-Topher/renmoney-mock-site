8cb0ef3eb1efdc45b884d1a77ab3a564ea3ec345
Jones Ogolo <jay.topher11@gmail.com>
2020-01-23 20:35:51 +0100

    created a hero page

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="hero_title col-6">
+        <h1>
+        Over 200,000 loans provided to individuals and small businesses in Nigeria.
+        </h1>
+        <a href="#" className="butn">Apply Now</a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
