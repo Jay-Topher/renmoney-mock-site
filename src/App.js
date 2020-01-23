546b321a613e97d14de2c459f0a647f607a93d2e
Jones Ogolo <jay.topher11@gmail.com>
2020-01-23 23:58:11 +0100

    mounted components
    Navbar, hero, first section, second section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './commons/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import FirstSection from './components/FirstSection/FirstSection';
+import SecondSection from './components/SecondSection/SecondSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <FirstSection />
+      <SecondSection />
     </div>
   );
 }
