37c566f13d601d540717fca46b1d8c7b333ae7fd
Jones Ogolo <jay.topher11@gmail.com>
2020-01-24 00:15:44 +0100

    made the apply button a component

---
+++ src/components/ApplyButton/ApplyButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ApplyButton.scss";
+
+const ApplyButton = () => {
+  return (
+    <a href="#" className="butn">
+      Apply Now
+    </a>
+  );
+};
+
+export default ApplyButton;
